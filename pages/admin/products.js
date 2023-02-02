19c0f6131e898c504f806014ecfae0202dece507
lotfi <lotfiaouane@gmail.com>
2023-02-02 14:30:08 +0100

    add and delete product

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -3,7 +3,8 @@ import Sidebar from '@/components/Sidebar';
 import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useReducer } from 'react';
-// import Layout from '../../components/Layout';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
 import { getError } from '../../utils/error';
 
 function reducer(state, action) {
@@ -14,16 +15,51 @@ function reducer(state, action) {
       return { ...state, loading: false, products: action.payload, error: '' };
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
+    case 'CREATE_REQUEST':
+      return { ...state, loadingCreate: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loadingCreate: false };
+    case 'CREATE_FAIL':
+      return { ...state, loadingCreate: false };
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true };
+    case 'DELETE_SUCCESS':
+      return { ...state, loadingDelete: false, successDelete: true };
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false };
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false };
     default:
       state;
   }
 }
 export default function AdminProductsScreen() {
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+  const router = useRouter();
+
+  const [
+    { loading, error, products, loadingCreate, successDelete, loadingDelete },
+    dispatch,
+  ] = useReducer(reducer, {
     loading: true,
     products: [],
     error: '',
   });
+
+  const createHandler = async () => {
+    if (!window.confirm('Are you sure?')) {
+      return;
+    }
+    try {
+      dispatch({ type: 'CREATE_REQUEST' });
+      const { data } = await axios.post(`/api/admin/products`);
+      dispatch({ type: 'CREATE_SUCCESS' });
+      toast.success('Product created successfully');
+      router.push(`/admin/product/${data.product._id}`);
+    } catch (err) {
+      dispatch({ type: 'CREATE_FAIL' });
+      toast.error(getError(err));
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,8 +72,27 @@ export default function AdminProductsScreen() {
       }
     };
 
-    fetchData();
-  }, []);
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (productId) => {
+    if (!window.confirm('Are you sure?')) {
+      return;
+    }
+    try {
+      dispatch({ type: 'DELETE_REQUEST' });
+      await axios.delete(`/api/admin/products/${productId}`);
+      dispatch({ type: 'DELETE_SUCCESS' });
+      toast.success('Product deleted successfully');
+    } catch (err) {
+      dispatch({ type: 'DELETE_FAIL' });
+      toast.error(getError(err));
+    }
+  };
   return (
     // <Layout title="Admin Products">
     <>
@@ -45,7 +100,17 @@ export default function AdminProductsScreen() {
       <div className="flex items-start flex-col md:flex-row md:gap-5">
         <Sidebar />
         <div className="py-5  md:p-5 md:w-4/5">
-          <h1 className="mb-4 text-xl">Products</h1>
+          <div className="flex items-center justify-between px-5 mt-5">
+            <h1 className="mb-4 text-xl font-semibold">Products</h1>
+            {loadingDelete && <div>Deleting item...</div>}
+            <button
+              disabled={loadingCreate}
+              onClick={createHandler}
+              className="primary-button mr-5 font-semibold"
+            >
+              {loadingCreate ? 'Loading' : '+ ADD'}
+            </button>
+          </div>
           {loading ? (
             <div>Loading...</div>
           ) : error ? (
@@ -97,9 +162,9 @@ export default function AdminProductsScreen() {
                       </td>
                       <td className=" p-5 ">
                         <button
+                          onClick={() => deleteHandler(product._id)}
+                          className="ml-3"
                           type="button"
-                          className="ml-4"
-                          onClick={() => {}}
                         >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
